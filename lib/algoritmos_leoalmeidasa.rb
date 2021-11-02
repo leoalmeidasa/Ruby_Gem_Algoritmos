da83f1144cdd43938764c27062ad48263e6f4288
Leonardo Almeida <leoalmeidasa@gmail.com>
2021-11-02 06:00:49 -0300

    build: Ruby Gem for Algoritmos

---
+++ lib/algoritmos_leoalmeidasa.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "algoritmos_leoalmeidasa/version"
+
+module AlgoritmosLeoalmeidasa
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/algoritmos_leoalmeidasa/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AlgoritmosLeoalmeidasa
+  VERSION = "0.1.0"
+end
